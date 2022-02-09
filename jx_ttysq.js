3a601d6d756e99fcfa14c7f8cd759279fbf8b4dc
yxgjay <yxgjay@live.com>
2022-02-09 14:40:49 +0800

    Update jx_ttysq.js

--- jx_ttysq.js
+++ jx_ttysq.js
@@ -58,9 +58,9 @@ const JD_API_HOST = `https://m.jingxi.com`;
             }
         }
     }
-    let res = await getAuthorShareCode('https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/ttysq2.json')
+    let res = await getAuthorShareCode('https://127.0.0.1/zero205/updateTeam/main/shareCodes/ttysq2.json')
     if (!res) {
-        res = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/ttysq2.json')
+        res = await getAuthorShareCode('https://127.0.0.1/zero205/updateTeam/main/shareCodes/ttysq2.json')
     }
     if (res) {
         authorCode = res.sort(() => 0.5 - Math.random())
